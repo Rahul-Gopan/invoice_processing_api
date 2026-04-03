0698cddfd7a7688d5ea79987923ee4a55c1edc19
Rahul Gopan <rahulgopan224@gmail.com>
2026-04-03 15:10:12 +0530

    Added Endpoint Exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -27,6 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->render(function (ValidationException $e, $request) {
             return response()->json([
                 'success' => false,
+                'status'  => 422,
                 'message' => 'Validation failed.',
                 'errors'  => $e->errors(),
             ], 422);
@@ -35,7 +37,25 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->render(function (ModelNotFoundException $e, $request) {
             return response()->json([
                 'success' => false,
+                'status'  => 404,
                 'message' => 'Record not found.',
             ], 404);
+        });
+
+
+        $exceptions->render(function (UnauthorizedHttpException $e, $request) {
+            return response()->json([
+                'success' => false,
+                'status'  => 401,
+                'message' => 'Unauthorized.',
+            ], 401);
+        });
+
+        $exceptions->render(function (Throwable $e, $request) {
+            return response()->json([
+                'success' => false,
+                'status'  => 500,
+                'message' => 'Internal server error.',
+            ], 500);
         });
     })->create();
